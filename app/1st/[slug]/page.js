acf5622717003ac2bf93940eb4477c769726f007
SASAGAWA Kiyoshi <sasagawa@kent-and-co.com>
2023-10-20 18:23:42 +0900

    fix global page.js

--- app/1st/[slug]/page.js
+++ app/1st/[slug]/page.js
@@ -5,6 +5,8 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkHtml from 'remark-html';
 
+import Image from 'next/image';
+
 import generateParams from '@/app/functions/generate-params';
 import './content.css';
 
@@ -24,22 +26,27 @@ export default async function BlogPost({ params }) {
     // ファイルの中身を取得
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const { data, content } = matter(fileContents);
-    const title = data.title; // 記事のタイトル
     const processedContent = await unified().use(remarkParse).use(remarkHtml).process(content);
     const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
 
     return (
         <div className="bg-ikyellow-200 dark:bg-ikpurple-400 py-12 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="flex-1 border-t border-b border-gray-200 py-10 px-5 flex flex-col gap-5 mt-8 md:mt-0">
-                    <div>{data.date}</div>
-                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-ikpurple-700 dark:text-ikyellow-300 sm:text-4xl">
-                        {data.title}
-                    </h1>
-                    <div
-                        className=""
-                        dangerouslySetInnerHTML={{ __html: contentHtml }}
-                    ></div>
+                <div className="md:flex gap-10 justify-between">
+                    <h2 className="text-ikyellow-800 dark:text-ikpurple-100 text-3xl font-bold tracking-tight sm:text-4xl m-0">
+                        <img src="/images/1st/smup-kv.png" width="1920" height="1080" alt="スーパマンタローカップ ルール ガチアサリ" className='w-full sm:w-60 mb-4' />
+                        <Image src="/images/SMUP-LOGO.svg" width="710" height="325" className="w-60" alt="いかすきー presents スーパーマンタローカップ" />
+                    </h2>
+                    <div className="flex-1 border-t border-b border-gray-200 py-10 px-5 flex flex-col gap-5 mt-8 md:mt-0">
+                        <div>{data.date}</div>
+                        <h1 className="mt-2 text-3xl font-bold tracking-tight text-ikpurple-700 dark:text-ikyellow-300 sm:text-4xl">
+                            {data.title}
+                        </h1>
+                        <div
+                            className=""
+                            dangerouslySetInnerHTML={{ __html: contentHtml }}
+                        ></div>
+                    </div>
                 </div>
             </div>
         </div>
